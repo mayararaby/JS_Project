8bcc0100781378f500aaa2ec17924c1d795214e1
mayararaby <mayararaby246@gmail.com>
2021-05-13 14:07:24 -0700

    edit path and navbar inlogin and sign up pages

--- js/signup.js
+++ js/signup.js
@@ -2,7 +2,7 @@ function signUp() {
     let userName = document.getElementById("orangeForm-name").value;
     let userEmail = document.getElementById("orangeForm-email").value;
     let userPassword = document.getElementById("orangeForm-pass").value;
-    if (userName.length < 5)
+    if (userName.length < 4)
         document.getElementById("errorName").innerHTML = "<div class='alert alert-danger' role='alert'>Please enter valid name</div>";
     else if (userPassword.length < 5)
         document.getElementById("errorPassword").innerHTML = "<div class='alert alert-danger' role='alert'>Your password is weak please choose another one more than 5 characters or numbers or symbol</div>";

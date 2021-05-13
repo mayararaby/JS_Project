ae921a48e41b2d976a9dd9d4580329c24c0cfc5f
mayararaby <mayararaby246@gmail.com>
2021-05-13 13:28:36 -0700

    login added

---
+++ js/signIn.js
@@ -0,0 +1,11 @@
+function signIn() {
+    let inputEmail = document.getElementById("orangeForm-email").value;
+    let inputPassword = document.getElementById("orangeForm-pass").value;
+    let storageEmail = localStorage.getItem("userEmail");
+    let storagePassword = localStorage.getItem("userPassword");
+    if (inputEmail == storageEmail && inputPassword == storagePassword) {
+        document.location.assign("index.html")
+    } else {
+        document.getElementById("wrongMessage").innerHTML = "<div class='alert alert-danger' role='alert'>You input wrong data your email or password is wrong</div>"
+    }
+}
